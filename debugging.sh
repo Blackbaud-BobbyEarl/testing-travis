d8405fe9ad0e88e421b9db9b6185fd22fe7d2671
Bobby Earl <bobby.earl@blackbaud.com>
2015-07-15 13:56:03 -0400

    Update debugging.sh

--- debugging.sh
+++ debugging.sh
@@ -1,4 +1,4 @@
-declare -a arr=("TRAVIS_PULL_REQUEST" "TRAVIS_OS_NAME" "TRAVIS_TAG")
+declare -a arr=('TRAVIS_PULL_REQUEST' 'TRAVIS_OS_NAME' 'TRAVIS_TAG' 'TRAVIS_JOB_NUMBER' 'TRAVIS_BUILD_NUMBER')
 for i in "${arr[@]}"
 do
    echo "$i: ${!i}"
