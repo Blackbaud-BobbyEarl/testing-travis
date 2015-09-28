e374ac675cf101e141657edf65e12426bb486632
Bobby Earl <bobby.earl@blackbaud.com>
2015-09-28 14:55:27 -0400

    Update gruntfile.js

--- gruntfile.js
+++ gruntfile.js
@@ -4,24 +4,38 @@ module.exports = function (grunt) {
   'use strict';
   
   grunt.registerTask('default', function () {
-    var props = [
-      'TRAVIS_REPO_SLUG',
-      'TRAVIS_SECURE_ENV_VARS',
-      'TRAVIS_BRANCH',
-      'TRAVIS_PULL_REQUEST',
-      'TRAVIS_OS_NAME',
-      'TRAVIS_TAG',
-      'TRAVIS_JOB_NUMBER',
-      'TRAVIS_BUILD_NUMBER',
-      'TRAVIS_COMMIT'
-    ];
+    var target = 'local',
+      props = [
+        'TRAVIS_REPO_SLUG',
+        'TRAVIS_SECURE_ENV_VARS',
+        'TRAVIS_BRANCH',
+        'TRAVIS_PULL_REQUEST',
+        'TRAVIS_OS_NAME',
+        'TRAVIS_TAG',
+        'TRAVIS_JOB_NUMBER',
+        'TRAVIS_BUILD_NUMBER',
+        'TRAVIS_COMMIT'
+      ];
+      
     console.log('RUNNING TEST: ' + process.env.TEST_NAME);
+    
     props.forEach(function (prop) {
       console.log(prop + ': ' + process.env[prop]);
     });
+
+    if (process.env.TRAVIS) {
+        if (process.env.TRAVIS_SECURE_ENV_VARS) {
+            if (process.env.TRAVIS_PULL_REQUEST === 'false') {
+                target = 'travis-push';
+            } else {
+                target = 'travis-pr-branch';
+            }
+        } else {
+            target = 'travis-pr-fork';
+        }
+    }
     
-    console.log('IS PULL REQUEST? ' + (process.env.TRAVIS_PULL_REQUEST !== 'false' ? 'yes' : 'no'));
-    
-    console.log("HAVE A GREAT DAY!");
+    console.log('TARGET: ' + target);
+
   });
 };
