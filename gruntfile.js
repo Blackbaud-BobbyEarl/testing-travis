d958897ecbbbd2f653eb1b1faa703c731b8dc9ce
Bobby Earl <bobby.earl@blackbaud.com>
2015-09-28 15:49:58 -0400

    Better testing for pr from fork/branch.

--- gruntfile.js
+++ gruntfile.js
@@ -2,7 +2,7 @@
 
 module.exports = function (grunt) {
   'use strict';
-  
+
   grunt.registerTask('default', function () {
     var target = 'local',
       props = [
@@ -16,15 +16,15 @@ module.exports = function (grunt) {
         'TRAVIS_BUILD_NUMBER',
         'TRAVIS_COMMIT'
       ];
-      
+
     console.log('RUNNING TEST: ' + process.env.TEST_NAME);
-    
+
     props.forEach(function (prop) {
       console.log(prop + ': ' + process.env[prop]);
     });
 
     if (process.env.TRAVIS) {
-        if (process.env.TRAVIS_SECURE_ENV_VARS === 'true') {
+        if (process.env.TRAVIS_REPO_SLUG === 'blackbaud/sky') {
             if (process.env.TRAVIS_PULL_REQUEST === 'false') {
                 target = 'travis-push';
             } else {
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
             target = 'travis-pr-fork';
         }
     }
-    
+
     console.log('TARGET: ' + target);
 
   });
