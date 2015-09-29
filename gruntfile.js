5c3303e44c1bc4ff37c54f2acd33cd06a46df6b5
Bobby Earl <bobby.earl@blackbaud.com>
2015-09-29 08:28:23 -0400

    Adding grunt bump

--- gruntfile.js
+++ gruntfile.js
@@ -1,41 +1,67 @@
 /*global module */
 
 module.exports = function (grunt) {
-  'use strict';
+    'use strict';
 
-  grunt.registerTask('default', function () {
-    var target = 'local',
-      props = [
-        'TRAVIS_REPO_SLUG',
-        'TRAVIS_SECURE_ENV_VARS',
-        'TRAVIS_BRANCH',
-        'TRAVIS_PULL_REQUEST',
-        'TRAVIS_OS_NAME',
-        'TRAVIS_TAG',
-        'TRAVIS_JOB_NUMBER',
-        'TRAVIS_BUILD_NUMBER',
-        'TRAVIS_COMMIT'
-      ];
-
-    console.log('RUNNING TEST: ' + process.env.TEST_NAME);
-
-    props.forEach(function (prop) {
-      console.log(prop + ': ' + process.env[prop] + ' (' + typeof process.env[prop] + ')');
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: [
+                    'bower.json',
+                    'package.json'
+                ],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['-a'],
+                createTag: false,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: true,
+                pushTo: 'origin',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
+        }
     });
 
-    if (process.env.TRAVIS) {
-        if (process.env.TRAVIS_SECURE_ENV_VARS === 'true') {
-            if (process.env.TRAVIS_PULL_REQUEST === 'false') {
-                target = 'travis-push';
+    grunt.registerTask('default', function () {
+        var target = 'local',
+            props = [
+            'TRAVIS_REPO_SLUG',
+            'TRAVIS_SECURE_ENV_VARS',
+            'TRAVIS_BRANCH',
+            'TRAVIS_PULL_REQUEST',
+            'TRAVIS_OS_NAME',
+            'TRAVIS_TAG',
+            'TRAVIS_JOB_NUMBER',
+            'TRAVIS_BUILD_NUMBER',
+            'TRAVIS_COMMIT'
+            ];
+
+        console.log('RUNNING TEST: ' + process.env.TEST_NAME);
+
+        props.forEach(function (prop) {
+            console.log(prop + ': ' + process.env[prop] + ' (' + typeof process.env[prop] + ')');
+        });
+
+        if (process.env.TRAVIS) {
+            if (process.env.TRAVIS_SECURE_ENV_VARS === 'true') {
+                if (process.env.TRAVIS_PULL_REQUEST === 'false') {
+                    target = 'travis-push';
+                } else {
+                    target = 'travis-pr-branch';
+                }
             } else {
-                target = 'travis-pr-branch';
+                target = 'travis-pr-fork';
             }
-        } else {
-            target = 'travis-pr-fork';
         }
-    }
 
-    console.log('TARGET: ' + target);
+        console.log('TARGET: ' + target);
 
-  });
+    });
+
+    grunt.loadNpmTasks('grunt-bump');
 };
