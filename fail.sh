52c1097d58a61239cfe6574bee91573fd2a6481b
Bobby Earl <bobby.earl@blackbaud.com>
2015-10-29 15:16:49 -0400

    Update fail.sh

--- fail.sh
+++ fail.sh
@@ -1,9 +1,9 @@
 # Can A comment go here?
 set -e
 
-#echo "No failing now"
+echo "No failing now"
 
-echo "Fail the build on purpose"
-exit 1
+#echo "Fail the build on purpose"
+#exit 1
 
-echo "Should not get here"
+echo "Should get here"
