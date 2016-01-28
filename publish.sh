431ab21c36e0fe7edc322a85f1578a029d559d4a
Bobby Earl <bobby.earl@blackbaud.com>
2016-01-28 11:02:25 -0500

    Updating version

--- publish.sh
+++ publish.sh
@@ -1,5 +1,5 @@
 set -e
 
-echo -e "blackbaud-skyux\n$NPM_PASSWORD\nsky-build-user@blackbaud.com" | npm login
+echo -e "blackbaud-bobbyearl\n$NPM_PASSWORD\nbobby.earl@blackbaud.com" | npm login
 npm whoami
 npm publish
