231f04e8c3a002469797a52d635edede09057935
Bobby Earl <bobby.earl@blackbaud.com>
2016-02-08 16:01:50 -0500

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -7,6 +7,6 @@ set -e
 export RELEASE_VERSION=1.2.3
 echo BEFORE
 cat README.md
-sed -i '' -E -e 's/blackbaudcdn.net\/skyux\/[0-9]+\.[0-9]+\.[0-9]+\//blackbaudcdn.net\/skyux\/'"$RELEASE_VERSION"'\//g' README.md
+sed -i '' -E -e 's/asdf/fdsa/g' README.md
 echo "AFTER"
 cat README.md
