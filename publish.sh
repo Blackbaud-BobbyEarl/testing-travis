96499a343114b74f085b7cb4c7ace7964b974007
Bobby Earl <bobby.earl@blackbaud.com>
2016-02-08 15:53:54 -0500

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,5 +1,9 @@
 set -e
 
-echo -e "blackbaud-bobbyearl\n$NPM_PASSWORD\nbobby.earl@blackbaud.com" | npm login
-npm whoami
-npm publish
+#echo -e "blackbaud-bobbyearl\n$NPM_PASSWORD\nbobby.earl@blackbaud.com" | npm login
+#npm whoami
+#npm publish
+
+export RELEASE_VERSION=1.2.3
+sed -i '' -E -e 's/blackbaudcdn.net\/skyux\/[0-9]+\.[0-9]+\.[0-9]+\//blackbaudcdn.net\/skyux\/'"$RELEASE_VERSION"'\//g' README.md
+echo README.md
