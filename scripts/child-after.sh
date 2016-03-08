b06531da5f20b41b71ae46b1be995fdc9b105ec3
Bobby Earl <bobby.earl@blackbaud.com>
2016-03-08 16:01:38 -0500

    testing

--- scripts/child-after.sh
+++ scripts/child-after.sh
@@ -1,1 +1,12 @@
 echo "BROWSER_STACK_PID is $BROWSER_STACK_PID"
+
+wget https://www.browserstack.com/browserstack-local/BrowserStackLocal-linux-x64.zip
+unzip BrowserStackLocal-linux-x64.zip
+./BrowserStackLocal -v -onlyAutomate -forcelocal $BROWSER_STACK_ACCESS_KEY &
+
+echo "--- Before ---"
+ps -ef | grep BrowserStackLocal
+pkill -f BrowserStackLocal
+
+echo "--- After ---"
+ps -ef | grep BrowserStackLocal
