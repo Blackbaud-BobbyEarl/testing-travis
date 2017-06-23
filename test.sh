3e5ba92282a4fa7c2f8a49d6ed5e76b0adfceff6
Bobby Earl <bobby.earl@blackbaud.com>
2017-06-22 20:23:57 -0400

    test

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+echo -e "Blackbaud - SKY UX Travis - After Success"
+
+# Necessary to stop pull requests from forks from running outside of Savage
+# Publish a tag to NPM
+if [[ $NPM_TOKEN ]]; then
+  echo -e "Using NPM_TOKEN"
+else if [[ $NPM_PASSWORD ]]; then
+  echo -e "USING NPM_PASSWORD"
+else
+  echo -c "No credentials provided"
+fi
