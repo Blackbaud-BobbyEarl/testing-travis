2acf44ccec000e864fad68662047aa19bbf5607e
Bobby Earl <bobby.earl@blackbaud.com>
2019-04-09 14:44:01 -0400

    No install

--- test.sh
+++ test.sh
@@ -3,17 +3,17 @@ set -e
 
 echo -e "Blackbaud - SKY UX Travis - After Success"
 
-printenv
+#printenv
 
 # Necessary to stop pull requests from forks from running outside of Savage
 # Publish a tag to NPM
-if [[ $NPM_TOKEN ]]; then
-  echo -e "Using NPM_TOKEN"
-elif [[ $NPM_PASSWORD ]]; then
-  echo -e "USING NPM_PASSWORD"
-else
-  echo -c "No credentials provided"
-fi
+#if [[ $NPM_TOKEN ]]; then
+#  echo -e "Using NPM_TOKEN"
+#elif [[ $NPM_PASSWORD ]]; then
+#  echo -e "USING NPM_PASSWORD"
+#else
+#  echo -c "No credentials provided"
+#fi
 
-gem install travis ruby-jq
+gem install travis
 travis raw /v3/build/$TRAVIS_BUILD_ID --json --skip-completion-check | jq -r '.created_by.login'
