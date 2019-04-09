8483db114f8c86794ca7b7a97a45828c23f2c9a8
Bobby Earl <bobby.earl@blackbaud.com>
2019-04-09 15:14:49 -0400

    uppercase

--- test.sh
+++ test.sh
@@ -3,6 +3,13 @@ set -e
 
 echo -e "Blackbaud - SKY UX Travis - After Success"
 
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
+
 #printenv
 
 # Necessary to stop pull requests from forks from running outside of Savage
@@ -15,16 +22,18 @@ echo -e "Blackbaud - SKY UX Travis - After Success"
 #  echo -c "No credentials provided"
 #fi
 SKYUX_TEAM=(
-  Blackbaud-BobbyEarl
-  Blackbaud-PaulCrowder
-  Blackbaud-TerryHelems
-  Blackbaud-SteveBrush
-  Blackbaud-AlexKingman
-  Blackbaud-TrevorBurch
+  BLACKBAUD-BOBBYEARL 
+  BLACKBAUD-TERRYHELEMS 
+  BLACKBAUD-STEVEBRUSH 
+  BLACKBAUD-ALEXKINGMAN 
+  BLACKBAUD-TREVORBURCH 
 )
 
 gem install travis
-createdBy=$(travis raw /v3/build/$TRAVIS_BUILD_ID --json --skip-completion-check | jq -r '.created_by.login')
+createdBy=$(travis raw /v3/build/$TRAVIS_BUILD_ID --json --skip-completion-check | jq -r '.created_by.login') | tr "[:upper:]"
 
-echo $createdBy
-echo $SKYUX_TEAM
+if [[ "${SKYUX_TEAM[@]}" =~ "${createdBy}" ]]; then
+  echo "${createdBy} has permission to release."
+else
+  echo "${createdBy} lacks permission to release.  Please contact the SKY UX team."
+fi
